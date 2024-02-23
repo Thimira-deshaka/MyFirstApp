aa352704f84955e3ac341c60630a3de4d42eed5a
Thimira Deshaka <thimiradeshakashan0220@gmail.com>
2024-02-24 01:58:25 +0530

    this is first step to auth and create log in and registration pages

--- REACT-FullStackApp/server/index.js
+++ REACT-FullStackApp/server/index.js
@@ -12,6 +12,8 @@ const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
 const commentsRouter = require("./routes/Comments");
 app.use("/Comments", commentsRouter);
+const usersRouter = require("./routes/Users");
+app.use("/auth", usersRouter);
 
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
